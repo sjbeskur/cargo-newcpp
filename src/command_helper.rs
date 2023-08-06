34d20e5092a6a4c7fab12b6e1631892d29ccc41c
sjbeskur <sam.beskur@gmail.com>
2023-08-06 00:18:10 -0600

    mvp gtest subcommand

---
+++ src/command_helper.rs
@@ -0,0 +1,25 @@
+use std::process::{Command, Stdio};
+use std::io::{BufRead, BufReader};
+
+pub fn dump_command(cmd: &mut Command){
+    cmd.stdout(Stdio::piped());  
+    let mut child = cmd.spawn().unwrap();  
+    let stdout = child.stdout.take().expect("Failed to get stdout");
+
+    // Create a buffer reader to read the output line by line
+    let reader = BufReader::new(stdout);
+
+    // Read and print the output in real-time
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(e) => eprintln!("Error reading line: {}", e),
+        }
+    }    
+
+    // Wait for the command to finish and check if it was successful
+    let status = child.wait().unwrap();
+    if !status.success() {
+        println!("Command failed with exit code: {:?}", status.code());
+    }
+}
